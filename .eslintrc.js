deb1b022fa47ddcc365efa09bd4bf8d2b597c354
Ahmad Raza <raza2157@gmail.com>
2018-09-18 02:02:44 -0400

    Updated files

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: [
+    "standard",
+    "plugin:react/recommended",
+    "prettier",
+    "prettier/react",
+    "prettier/standard"
+  ],
+  plugins: ["react", "prettier", "standard"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    es6: true,
+    node: true
+  },
+  rules: {
+    "prettier/prettier": "error"
+  },
+  globals: {}
+}
