b16e6c6945e28c8ebc95c742df25633ca670b4e7
Ahmad Raza <raza2157@gmail.com>
2018-09-19 00:53:01 -0400

    Updated index file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Head from "next/head";
 
 const Index = () => (
   <div>
-    <p>{"Tracko, a reseller's best friend. :)"}</p>
+    <Head>
+      <title>Tracko - An inventory and sales tracking app</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>Hello world!</p>
+    <form>
+      <label>
+        Name:
+        <input type="text" name="name" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   </div>
 );
 
