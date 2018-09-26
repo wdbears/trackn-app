307519ca577c459bab2efa08fad6dec3522e126b
Ahmad Raza <raza2157@gmail.com>
2018-09-26 12:43:43 -0400

    Seperated stylesheets from components

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "../logo.svg";
-import "./App.css";
+import "../style/App.css";
 
 class App extends Component {
   render() {
---
+++ src/component/Form.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../style/Form.css";
+
+class Form extends Component {
+  render() {
+    return <div className="Form" />;
+  }
+}
+
+export default Form;
