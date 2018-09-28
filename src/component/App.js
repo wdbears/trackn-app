7ff0ce94d65e23cbfc84b368302ebc1e37a53462
Ahmad Raza <raza2157@gmail.com>
2018-09-28 00:40:16 -0400

    Updated color scheme, more styling

--- src/component/App.js
+++ src/component/App.js
@@ -2,18 +2,22 @@ import React, { Component } from "react";
 import logo from "../logo.svg";
 import "../style/App.css";
 
+import Form from "./Form";
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Tracko</h1>
+          <h1 className="App-title">t r a c k n</h1>
         </header>
         <p className="App-intro">
-          {"An inventory and sales tracking application."}
+          {"An inventory and sales tracking application..."}
+          <p className="App-description">{"and a reseller's best friend."}</p>
         </p>
-        <p className="Description">{"A reseller's best friend. :)"}</p>
+        <div>
+          <Form />
+        </div>
       </div>
     );
   }
--- src/component/Form.js
+++ src/component/Form.js
@@ -3,7 +3,32 @@ import "../style/Form.css";
 
 class Form extends Component {
   render() {
-    return <div className="Form" />;
+    return (
+      <div className="Form">
+        <table>
+          <thead>
+            <tr>
+              <th>Devs</th>
+              <th>Role</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Ahmad</td>
+              <td>Overlord</td>
+            </tr>
+            <tr>
+              <td>Luis</td>
+              <td>Backboi</td>
+            </tr>
+            <tr>
+              <td>Jarman</td>
+              <td>Funko COOK</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
   }
 }
 
